04939ae71a47f1404f407660f890d270ad137378
Bibesh Manandhar <bibesh.manandhar@gmail.com>
2018-07-12 20:10:06 +0545

    Added Splash Screen Example

---
+++ SplashScreenExample/app/src/main/java/com/planetinnovative/splashscreenexample/MainActivity.java
@@ -0,0 +1,13 @@
+package com.planetinnovative.splashscreenexample;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
---
+++ SplashScreenExample/app/src/main/java/com/planetinnovative/splashscreenexample/SplashScreenActivity.java
@@ -0,0 +1,25 @@
+package com.planetinnovative.splashscreenexample;
+
+import android.content.Intent;
+import android.os.Handler;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class SplashScreenActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_splash_screen);
+        Runnable runnable = new Runnable() {
+            @Override
+            public void run() {
+                Intent intent = new Intent(SplashScreenActivity.this, MainActivity.class);
+                startActivity(intent);
+                finish();
+            }
+        };
+        Handler handler = new Handler();
+        handler.postDelayed(runnable, 2000);
+    }
+}
