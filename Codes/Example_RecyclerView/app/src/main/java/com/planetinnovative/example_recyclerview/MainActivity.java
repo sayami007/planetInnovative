86a712a59db45875984e81d5f0c0e2c70dfe08d4
Bibesh Manandhar <bibesh.manandhar@gmail.com>
2018-07-13 18:44:23 +0545

    RecyclerView

--- Codes/Example_RecyclerView/app/src/main/java/com/planetinnovative/example_recyclerview/MainActivity.java
+++ Codes/Example_RecyclerView/app/src/main/java/com/planetinnovative/example_recyclerview/MainActivity.java
@@ -3,12 +3,17 @@ package com.planetinnovative.example_recyclerview;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 
+import android.support.v7.widget.DividerItemDecoration;
+import android.support.v7.widget.GridLayoutManager;
 import android.support.v7.widget.LinearLayoutManager;
 import android.support.v7.widget.RecyclerView;
 
 import java.util.ArrayList;
 
+import static android.content.res.Configuration.ORIENTATION_PORTRAIT;
+
 public class MainActivity extends AppCompatActivity {
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -20,22 +25,24 @@ public class MainActivity extends AppCompatActivity {
         myApp.add(new MyApplication(R.drawable.im2, "Facebook"));
         myApp.add(new MyApplication(R.drawable.im3, "Instagram"));
         myApp.add(new MyApplication(R.drawable.im1, "Instagram"));
-        myApp.add(new MyApplication(R.drawable.im2, "Instagram"));
+        myApp.add(new MyApplication(R.drawable.im2, "App Instagram"));
         myApp.add(new MyApplication(R.drawable.im3, "Instagram"));
-        myApp.add(new MyApplication(R.drawable.im1, "Instagram"));
-        myApp.add(new MyApplication(R.drawable.im3, "Instagram"));
+        myApp.add(new MyApplication(R.drawable.im1, "Mock App"));
+        myApp.add(new MyApplication(R.drawable.im3, "Hello World"));
         myApp.add(new MyApplication(R.drawable.im2, "Instagram"));
         myApp.add(new MyApplication(R.drawable.im1, "Instagram"));
         myApp.add(new MyApplication(R.drawable.im2, "Instagram"));
         myApp.add(new MyApplication(R.drawable.im3, "Instagram"));
         myApp.add(new MyApplication(R.drawable.im2, "Instagram"));
         //
-
-
-
         RecyclerView recyclerView = findViewById(R.id.recyclerView);
-        recyclerView.setLayoutManager(new LinearLayoutManager(getApplicationContext()));
+        if(getResources().getConfiguration().orientation == ORIENTATION_PORTRAIT )
+            recyclerView.setLayoutManager(new LinearLayoutManager(getApplicationContext()));
+        else
+            recyclerView.setLayoutManager(new GridLayoutManager(getApplicationContext(),2));
         recyclerView.setAdapter(new CustomAdapter(getApplicationContext(), myApp));
+        DividerItemDecoration decoration = new DividerItemDecoration(getApplicationContext(),DividerItemDecoration.VERTICAL);
+        recyclerView.addItemDecoration(decoration);
 
     }
 }
--- Codes/Example_RecyclerView/app/src/main/java/com/planetinnovative/example_recyclerview/MyApplication.java
+++ Codes/Example_RecyclerView/app/src/main/java/com/planetinnovative/example_recyclerview/MyApplication.java
@@ -17,4 +17,5 @@ public class MyApplication {
         return appTitle;
     }
 
+
 }
